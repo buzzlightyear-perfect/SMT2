c73d78a995424888e2b63e602422c08073da78ff
Bhaskoro V <bhaskorou@gmail.com>
2026-06-27 10:24:38 +0700

    web materi php

---
+++ web-materi/tugas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello world";
+    print "belajar PHP";
+
+    var_dump(5 == "5");  
+    var_dump(5 === "5");  
+    ?>
+</body>
+</html>
